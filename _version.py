cee1fa60d946f68845248f398840ecb9b05387cf
Rogers Hornsby <rogers.hornsby@whitehouse.gov>
2023-11-10 21:28:39 -0500

    Added regexes for all state/terr

---
+++ _version.py
@@ -0,0 +1,5 @@
+__desc__="""
+As some have pointed out, this isn't going to categorize every number, but this will give you an idea of how SSA.gov has historically
+defined number allocations on a per-state/territory basis.
+"""
+__version__="1.0.0"
